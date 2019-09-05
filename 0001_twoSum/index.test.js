905f7612fc007453faa4425c5ce5218ef6f95c0f
takeu <tony40508@gmail.com>
2019-09-05 19:38:07 +0800

    test: 0001

---
+++ 0001_twoSum/index.test.js
@@ -0,0 +1,7 @@
+import { twoSum } from '../0001_twoSum/index'
+
+test('0001_twoSum', () => {
+  expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1])
+  expect(twoSum([2, 7, 11, 15], 18)).toEqual([1, 2])
+  expect(twoSum([2, 7, 11, 15], 17)).toEqual([0, 3])
+})
