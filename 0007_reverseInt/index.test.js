4f2edd57406998e637b6d7dc060203e0c28b4c06
takeu <tony40508@gmail.com>
2019-09-06 23:48:13 +0800

    test: 0007

---
+++ 0007_reverseInt/index.test.js
@@ -0,0 +1,8 @@
+import { reverseInt } from '../0007_reverseint/index'
+
+test('0007_reverseInt', () => {
+  expect(reverseInt(123)).toEqual(321)
+  expect(reverseInt(1230)).toEqual(321)
+  expect(reverseInt(-Math.pow(2, 31))).toEqual(0)
+  expect(reverseInt(Math.pow(2, 31))).toEqual(0)
+})
