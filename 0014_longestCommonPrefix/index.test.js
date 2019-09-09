eedfcacbbd3eddd5d0b951774d57cd20d95efd07
takeu <tony40508@gmail.com>
2019-09-09 22:39:53 +0800

    test: 0014

---
+++ 0014_longestCommonPrefix/index.test.js
@@ -0,0 +1,9 @@
+import { longestCommonPrefix } from './index'
+
+test('0014_longestCommonPrefix', () => {
+  expect(longestCommonPrefix(['flower', 'flow', 'flight'])).toEqual('fl')
+  expect(longestCommonPrefix(['dog', 'racecar', 'car'])).toEqual('')
+  expect(longestCommonPrefix([''])).not.toBeUndefined()
+  expect(longestCommonPrefix(['a'])).not.toBeUndefined()
+  expect(longestCommonPrefix(['', ''])).toEqual('')
+})
