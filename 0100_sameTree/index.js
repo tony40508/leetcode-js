c916e0fa5e326009bf468a3a6f67b8473983a9cc
weiyu <tony40508@gmail.com>
2019-12-08 16:22:57 +0800

    finish: 0100_sameTree

---
+++ 0100_sameTree/index.js
@@ -0,0 +1,7 @@
+function isSameTree(p, q) {
+  if (p === null && q === null) return true
+  if (p === null || q === null) return false
+
+  if (p.val !== q.val) return false
+  return isSameTree(p.right, q.right) && isSameTree(p.left, q.left)
+}
