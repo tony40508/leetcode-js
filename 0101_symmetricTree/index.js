01d9c12db6ce069a99dbdfe8d5e047314998511e
weiyu <tony40508@gmail.com>
2019-12-08 18:04:45 +0800

    finish: 0101_symmetricTree

---
+++ 0101_symmetricTree/index.js
@@ -0,0 +1,16 @@
+// Depth-First search
+export function isSymmetric(root) {
+  if (!root) return true
+
+  function isSym(rootLeft, rootRight) {
+    if (rootLeft === null && rootRight === null) return true
+    if (rootLeft === null || rootRight === null) return false
+    if (rootLeft?.val !== rootRight?.val) return false
+    return (
+      // rootLeft.val === rootRight.val
+      isSym(rootLeft.left, rootRight.right) &&
+      isSym(rootLeft.right, rootRight.left)
+    )
+  }
+  return isSym(root.left, root.right)
+}
