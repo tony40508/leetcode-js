20c42de531709df5b0f6107f4afb32524183e08b
weiyu <tony40508@gmail.com>
2019-12-19 00:48:44 +0800

    finish: 0098_ValidateBinarySearchTree

---
+++ 0098_ValidateBinarySearchTree/index.js
@@ -0,0 +1,16 @@
+function isValidBST(root) {
+  function isValid(root, min, max) {
+    // must at first line to prevent null
+    if (!root) return true
+    const val = root.val
+    const left = root.left
+    const right = root.right
+    if (val <= min || val >= max) return false
+    // left should be smaller than val(parent), so put val to max
+    // right should be bigger than val(parent), so put val to min
+    if (!isValid(left, min, val)) return false
+    if (!isValid(right, val, max)) return false
+    return true
+  }
+  return isValid(root, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)
+}
