ccf215efb14fb8fd09e53fe4066a330d134c4311
weiyu <tony40508@gmail.com>
2019-12-24 00:19:12 +0800

    finish: 0838_PushDominoes

---
+++ 0838_PushDominoes/index.js
@@ -0,0 +1,36 @@
+function pushDominoes(dominoes) {
+  const result = []
+  const length = dominoes.length
+  const recordArray = [...new Array(length)].fill(0)
+
+  let record = 0
+  // to 'R'
+  for (let i = 0; i < length; i++) {
+    if (dominoes[i] === 'R') {
+      record = length // assign biggest number
+    } else if (dominoes[i] === 'L') {
+      record = 0 // conteract to 'R'
+    } else {
+      record = Math.max(record - 1, 0) // at least > 0
+    }
+    recordArray[i] = recordArray[i] + record
+  }
+  // to 'L'
+  for (let i = length - 1; i >= 0; i--) {
+    if (dominoes[i] === 'L') {
+      record = length
+    } else if (dominoes[i] === 'R') {
+      record = 0
+    } else {
+      record = Math.max(record - 1, 0)
+    }
+    recordArray[i] = recordArray[i] - record
+  }
+  return recordArray
+    .map(record => {
+      if (record > 0) return 'R'
+      if (record < 0) return 'L'
+      if (record === 0) return '.'
+    })
+    .join('')
+}
