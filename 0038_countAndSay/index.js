5c99bb5dda96bcb8601ad28f3985251541a0e249
weiyu <tony40508@gmail.com>
2020-02-09 18:56:41 +0800

    add: logs

--- 0038_countAndSay/index.js
+++ 0038_countAndSay/index.js
@@ -4,10 +4,12 @@ const countAndSay = n => {
     let strArray = [...str]
     let count = 1
     str = ''
+    console.log(strArray)
 
     for (let j = 0; j < strArray.length; j++) {
       if (strArray[j] !== strArray[j + 1]) {
         str += count + strArray[j]
+        console.log(count, str)
         count = 1
       } else {
         count++
@@ -16,3 +18,19 @@ const countAndSay = n => {
   }
   return str
 }
+
+console.log(countAndSay(5))
+// ["1"]
+// 1, "11"
+// ========================
+// ["1", "1"]
+// 2, "21"
+// ========================
+// ["2", "1"]
+// 1, "12"
+// 1, "1211"
+// ========================
+// ["1", "2", "1", "1"]
+// 1, "11"
+// 1, "1112"
+// 2, "111221"
