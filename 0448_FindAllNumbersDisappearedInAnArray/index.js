3e1e340a9297c1bddbab3c8e4e8340263f1827cb
weiyu <tony40508@gmail.com>
2020-02-16 23:20:06 +0800

    refactor: use cyclic sort

--- 0448_FindAllNumbersDisappearedInAnArray/index.js
+++ 0448_FindAllNumbersDisappearedInAnArray/index.js
@@ -1,3 +1,4 @@
+// map
 function findDisappearedNumbers(nums) {
   const length = nums.length
   if (!length) return []
@@ -10,3 +11,25 @@ function findDisappearedNumbers(nums) {
   }
   return result
 }
+// cyclic sort
+function findDisappearedNumbers(nums) {
+  const length = nums.length
+  if (!length) return []
+
+  nums.forEach(num => {
+    const NUM = Math.abs(num)
+    if (nums[NUM - 1] > 0) {
+      nums[NUM - 1] *= -1
+    }
+  })
+  let result = []
+  for (let i = 1; i <= length; i++) {
+    if (nums[i - 1] > 0) {
+      result = [...result, i]
+    }
+  }
+  return result
+}
+// input: [4,3,2,7,8,2,3,1]
+// nums: [ -4, -3, -2, -7, 8, 2, -3, -1 ]
+// result: [5,6]
