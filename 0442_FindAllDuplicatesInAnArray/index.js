9f469316f64a4fdf75c530154aeb7dfd134a21c2
weiyu <tony40508@gmail.com>
2020-02-16 10:53:08 +0800

    finish: 0442_FindAllDuplicatesInAnArray

---
+++ 0442_FindAllDuplicatesInAnArray/index.js
@@ -0,0 +1,12 @@
+function findDuplicates(nums) {
+  let result = []
+  for (const num of nums) {
+    const NUM = Math.abs(num)
+    if (nums[NUM - 1] < 0) {
+      result = [...result, NUM] // not appear
+    } else {
+      nums[NUM - 1] = nums[NUM - 1] * -1 // have appeared
+    }
+  }
+  return result
+}
