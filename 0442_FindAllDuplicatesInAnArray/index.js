361bcea8b2d059b8e3c68e0807967fbc9554fe37
weiyu <tony40508@gmail.com>
2020-02-16 23:22:37 +0800

    modify: comments

--- 0442_FindAllDuplicatesInAnArray/index.js
+++ 0442_FindAllDuplicatesInAnArray/index.js
@@ -3,13 +3,12 @@ function findDuplicates(nums) {
   for (const num of nums) {
     const NUM = Math.abs(num)
     if (nums[NUM - 1] < 0) {
-      result = [...result, NUM] // not appear
+      result = [...result, NUM] // duplicates
     } else {
-      nums[NUM - 1] = nums[NUM - 1] * -1 // have appeared
+      nums[NUM - 1] = nums[NUM - 1] * -1 // first appeared num * -1
     }
   }
   return result
 }
-
 // space complexity: O(1)
 // time complexity: O(N)
