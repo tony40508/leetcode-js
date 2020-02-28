f8ec7330c7a2a3331dd624cc0c3ccdb32192ebed
weiyu <tony40508@gmail.com>
2020-02-28 17:08:51 +0800

    refactor: two pointers and comments

---
+++ 0027_RemoveElement/index.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+
+// splice, unshift, shift need to re-order elements (less effective operations)
+function function(nums, val) {
+  for (let i = nums.length; i > 0; i--) {
+    if (nums[i] === val) {
+      nums.splice(i, 1)
+    }
+  }
+}
+
+function removeElement(nums, val) {
+  let start = 0
+  let end = nums.length - 1
+
+  while (start <= end) {
+    if (nums[end] === val) {
+      nums.pop()
+      end--
+      continue
+    }
+    if (nums[start] === val) {
+      // move `end` item to `start` and skip it afterward (start++)
+      // since `end` is checked first (it's safe)
+      // then remove last one to pretend removing `start` (prevent re-ordering)
+      nums[start] = nums[end]
+      nums.pop()
+      start++
+      end--
+      continue
+    }
+    start++
+  }
+  // order is not important since we only need to return length
+  return nums.length
+}
