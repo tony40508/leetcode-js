e3f520fe20483f3451b45f22f97f7c2920c2fc06
weiyu <tony40508@gmail.com>
2020-02-29 21:03:33 +0800

    add: comments

--- 0567_PermutationInString/index.js
+++ 0567_PermutationInString/index.js
@@ -11,12 +11,14 @@ function checkInclusion(s1, s2) {
   for (const s of s1) {
     s1Map.set(s, (s1Map.get(s) || 0) + 1)
   }
+  console.log('s1Map', s1Map)
 
   let start = 0
   let end = 0
   const slidingWindows = new Map()
 
   while (end < s2Length) {
+    console.log('============')
     // length > s1Length start moving slidingWindows
     if (end - start + 1 > s1Length) {
       // move left element
@@ -24,11 +26,15 @@ function checkInclusion(s1, s2) {
       start++
     }
     slidingWindows.set(s2[end], (slidingWindows.get(s2[end]) || 0) + 1)
+    console.log(slidingWindows)
+    console.log('end', end)
+    console.log('start', start)
     if (end - start + 1 === s1Length && isEqual(s1Map, slidingWindows)) {
       // subString(slidingWindows) is equal to s1(s1Map)
       return true
     }
     end++
+    console.log('============')
   }
   return false
 }
@@ -41,3 +47,32 @@ function isEqual(map1, map2) {
   }
   return true
 }
+
+// "ab"
+// "eidbaooo"
+
+// s1Map Map { 'a' => 1, 'b' => 1 }
+// ============
+// Map { 'e' => 1 }
+// end 0
+// start 0
+// ============
+// ============
+// Map { 'e' => 1, 'i' => 1 }
+// end 1
+// start 0
+// ============
+// ============
+// Map { 'e' => 0, 'i' => 1, 'd' => 1 }
+// end 2
+// start 1
+// ============
+// ============
+// Map { 'e' => 0, 'i' => 0, 'd' => 1, 'b' => 1 }
+// end 3
+// start 2
+// ============
+// ============
+// Map { 'e' => 0, 'i' => 0, 'd' => 0, 'b' => 1, 'a' => 1 }
+// end 4
+// start 3
