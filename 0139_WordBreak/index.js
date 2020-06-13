fecd288c27f4e072db2eff0c463b58c1d74bccf3
weiyu <tony40508@gmail.com>
2020-06-13 18:09:29 +0800

    refactor: replace map with includes()

--- 0139_WordBreak/index.js
+++ 0139_WordBreak/index.js
@@ -4,11 +4,6 @@
  * @return {boolean}
  */
 function wordBreak(s, wordDict) {
-  const map = wordDict.reduce((acc, cur) => {
-    acc[cur] = true
-    return acc
-  }, {})
-
   // dp[0] = true
   const dp = Array.from({ length: s.length + 1 }, (_, i) =>
     i === 0 ? true : false
@@ -18,7 +13,7 @@ function wordBreak(s, wordDict) {
   for (let i = 1; i <= s.length; i++) {
     for (let j = 0; j <= i - 1; j++) {
       // dp[j] (left) && map[s.substring(j, i) (right)
-      if (dp[j] && map[s.substring(j, i)]) {
+      if (dp[j] && wordDict.includes(s.substring(j, i))) {
         dp[i] = true
         break
       }
