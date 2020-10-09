270cd6505618ac4ae4c79ad6245aa4afba6ea96d
weiyu <tony40508@gmail.com>
2020-10-09 12:39:04 +0800

    finish(easy): 0226_InvertBinaryTree

---
+++ 0226_InvertBinaryTree/index.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+
+/** DFS */
+function invertTree(root) {
+  if (!root) return null
+  ;[root.left, root.right] = [root.right, root.left]
+
+  invertTree(root.left)
+  invertTree(root.right)
+
+  return root
+}
+
+/** BFS */
+function invertTree(root) {
+  if (!root) return null
+
+  const queue = []
+  queue.push(root)
+
+  while (queue.length > 0) {
+    const node = queue.pop()
+    ;[node.left, node.right] = [node.right, node.left]
+    if (node.left) queue.push(node.left)
+    if (node.right) queue.push(node.right)
+  }
+
+  return root
+}
