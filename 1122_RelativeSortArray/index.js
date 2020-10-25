c25a93eb28dafe392543e170f821bd70e32b7d69
weiyu <tony40508@gmail.com>
2020-10-25 16:09:59 +0800

    refactor: with map for fixed order of keys

--- 1122_RelativeSortArray/index.js
+++ 1122_RelativeSortArray/index.js
@@ -4,26 +4,55 @@
  * @return {number[]}
  */
 function relativeSortArray(arr1, arr2) {
-  const map = arr1.reduce((acc, cur) => {
-    acc[cur] ? acc[cur]++ : (acc[cur] = 1)
-    return acc
-  }, {})
+  const cache = new Map()
 
-  const result = []
+  arr1.sort((a, b) => a - b)
 
-  arr2.forEach((num) => {
-    while (map[num] !== 0) {
-      result.push(num)
-      map[num]--
+  arr2.forEach((item) => {
+    cache.set(item, 0)
+  })
+
+  arr1.forEach((item) => {
+    if (cache.get(item)) {
+      const count = cache.get(item)
+      cache.set(item, count + 1)
+    } else {
+      cache.set(item, 1)
     }
   })
 
-  Object.keys(map).forEach((num) => {
-    while (map[num] !== 0) {
-      result.push(num)
-      map[num]--
-    }
+  const result = []
+
+  cache.forEach((val, key) => {
+    result.push(...repeat({ item: key, times: val }))
   })
 
   return result
 }
+
+const repeat = ({ item, times }) => new Array(times).fill(item)
+
+// function relativeSortArray(arr1, arr2) {
+//   const map = arr1.reduce((acc, cur) => {
+//     acc[cur] ? acc[cur]++ : (acc[cur] = 1)
+//     return acc
+//   }, {})
+
+//   const result = []
+
+//   arr2.forEach((num) => {
+//     while (map[num] !== 0) {
+//       result.push(num)
+//       map[num]--
+//     }
+//   })
+
+//   Object.keys(map).forEach((num) => {
+//     while (map[num] !== 0) {
+//       result.push(num)
+//       map[num]--
+//     }
+//   })
+
+//   return result
+// }
