ef7f3cc65ae1807e067e0faf8f5219ab98e2de62
weiyu <tony40508@gmail.com>
2020-12-26 17:35:18 +0800

    refactor: 0055

--- 0055_JumpGame/index.js
+++ 0055_JumpGame/index.js
@@ -3,28 +3,42 @@
  * @return {boolean}
  */
 function canJump(nums) {
-  const l = nums.length
-  const lastIndex = l - 1
+  if (nums.length < 2) return true
 
-  if (l < 2) return true
+  let max = 0
 
-  const a = Array.from({ length: l }, () => false)
-  a[lastIndex] = true
-
-  // right to left (reduce complexity)
-  for (let i = lastIndex - 1; i >= 0; i--) {
-    if (i + nums[i] >= lastIndex) {
-      a[i] = true
-      continue
-    }
-    // if can't reach the last index, it should depend on right element (within max jump length it can reach)
-    for (let j = nums[i]; j >= 1; j--) {
-      if (a[i + j]) {
-        a[i] = true
-        break
-      }
-    }
+  for (const [i, num] of nums.entries()) {
+    if (i > max) return false
+    max = Math.max(max, num + i)
+    if (max >= nums.length - 1) return true
   }
-
-  return a[0]
 }
+
+console.log(canJump([3, 2, 1, 0, 4]))
+
+// function canJump(nums) {
+//   const l = nums.length
+//   const lastIndex = l - 1
+
+//   if (l < 2) return true
+
+//   const a = Array.from({ length: l }, () => false)
+//   a[lastIndex] = true
+
+//   // right to left (reduce complexity)
+//   for (let i = lastIndex - 1; i >= 0; i--) {
+//     if (i + nums[i] >= lastIndex) {
+//       a[i] = true
+//       continue
+//     }
+//     // if can't reach the last index, it should depend on right element (within max jump length it can reach)
+//     for (let j = nums[i]; j >= 1; j--) {
+//       if (a[i + j]) {
+//         a[i] = true
+//         break
+//       }
+//     }
+//   }
+
+//   return a[0]
+// }
