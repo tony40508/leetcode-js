bfa5cc1bbb44d4a765dc68aa0964ec00b39667fa
weiyu <tony40508@gmail.com>
2021-01-28 20:38:41 +0800

    fixup! test: fix test cases

--- 0007_reverseInt/index.js
+++ 0007_reverseInt/index.js
@@ -1,4 +1,4 @@
-export const reverseInt = (x) => {
+const reverseInt = (x) => {
   const min = -Math.pow(2, 31)
   const max = Math.pow(2, 31) - 1
   const sign = x > 0 ? 1 : -1
@@ -9,3 +9,5 @@ export const reverseInt = (x) => {
   }
   return reverseNum
 }
+
+export default reverseInt
