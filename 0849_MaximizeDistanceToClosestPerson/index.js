abfd8280ff5c0914d89846d3d6b9d2322a907f05
Wei Chen <tony40508@gmail.com>
2021-02-12 12:06:15 +0800

    finish(mid): 0849_MaximizeDistanceToClosestPerson

---
+++ 0849_MaximizeDistanceToClosestPerson/index.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} seats
+ * @return {number}
+ */
+function maxDistToClosest(seats) {
+  // first person <-> first position
+  let prev
+  for (let i = 0; i < seats.length; i++) {
+    if (seats[i] === 1) {
+      prev = i
+      break
+    }
+  }
+
+  let result = prev
+  for (let j = prev + 1; j < seats.length; j++) {
+    if (seats[j] === 0) continue
+    result = Math.max(result, Math.floor((j - prev) / 2))
+    prev = j
+  }
+
+  // last person <-> last position
+  for (let k = seats.length - 1; k >= 0; k--) {
+    if (seats[k] === 1) {
+      prev = k
+      break
+    }
+  }
+
+  return Math.max(result, seats.length - 1 - prev)
+}
