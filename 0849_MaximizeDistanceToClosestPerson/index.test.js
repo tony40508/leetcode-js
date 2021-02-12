a40bb658c20a6f210f88722a51d0cb64b87a1676
Wei Chen <tony40508@gmail.com>
2021-02-12 16:01:45 +0800

    fixup! test: 0849_MaximizeDistanceToClosestPerson

--- 0849_MaximizeDistanceToClosestPerson/index.test.js
+++ 0849_MaximizeDistanceToClosestPerson/index.test.js
@@ -4,10 +4,10 @@ describe('maxDistToClosest', () => {
   test('normal case: [1, 0, 0, 0, 1, 0, 1]', () => {
     expect(maxDistToClosest([1, 0, 0, 0, 1, 0, 1])).toBe(2)
   })
-  test('edge case: [1, 0, 0, 0]', () => {
+  test('edge case(distance between last person and last position): [1, 0, 0, 0]', () => {
     expect(maxDistToClosest([1, 0, 0, 0])).toBe(3)
   })
-  test('edge case: [0, 1]', () => {
+  test('edge case(distance between first person and first position): [0, 1]', () => {
     expect(maxDistToClosest([0, 1])).toBe(1)
   })
 })
