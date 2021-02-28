1716f75f5f923bd75f11ad1049669597608996c9
Wei Chen <tony40508@gmail.com>
2021-02-28 16:46:44 +0800

    finish(mid): 0846_HandOfStraights

---
+++ 0846_HandOfStraights/index.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} hand
+ * @param {number} W
+ * @return {boolean}
+ */
+function isNStraightHand(hand, W) {
+  const countsMap = hand.reduce((acc, cur) => {
+    if (Number.isInteger(acc[cur])) {
+      acc[cur]++
+    } else {
+      acc[cur] = 1
+    }
+    return acc
+  }, {})
+
+  const sortedHand = [...new Set(hand)].sort((a, b) => a - b)
+
+  for (const card of sortedHand) {
+    const count = countsMap[card]
+    // e.g. [1, 2, 3, 6, 2, 3, 4, 7, 8]
+    // 4 will be 0 so we need to skip it.
+    if (count === 0) continue
+
+    for (let i = card; i < card + W; i++) {
+      if (!Number.isInteger(countsMap[i])) {
+        return false
+      }
+      // should minus count instead of 1
+      // e.g. [1, 1, 2, 2, 3, 3] count is 2
+      countsMap[i] -= count
+      if (countsMap[i] < 0) return false
+    }
+  }
+
+  return true
+}
+
+isNStraightHand([1, 1, 2, 2, 3, 3], 3)
