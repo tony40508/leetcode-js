566f5ef9482b8d0ddac705f48f6a6d7298518218
Wei Chen <tony40508@gmail.com>
2021-07-04 18:22:51 +0800

    refactor: with map

--- 0448_FindAllNumbersDisappearedInAnArray/index.js
+++ 0448_FindAllNumbersDisappearedInAnArray/index.js
@@ -1,22 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
 // map
 function findDisappearedNumbers(nums) {
   const length = nums.length
   if (!length) return []
 
-  const map = {}
-  nums.forEach(num => (map[num] = true))
-  let result = []
+  const result = []
+  const map = new Map()
+
+  nums.forEach((num) => {
+    map.set(num, true)
+  })
+
   for (let i = 1; i <= length; i++) {
-    if (!map[i]) result = [...result, i]
+    if (!map.has(i)) {
+      result.push(i)
+    }
   }
+
   return result
 }
+
 // cyclic sort
 function findDisappearedNumbers(nums) {
   const length = nums.length
   if (!length) return []
 
-  nums.forEach(num => {
+  nums.forEach((num) => {
     const NUM = Math.abs(num)
     if (nums[NUM - 1] > 0) {
       nums[NUM - 1] *= -1
