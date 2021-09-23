d00f6303e2a416f3e8572a88cdcb2f411a18aae8
Wei Chen <tony40508@gmail.com>
2021-09-24 04:53:25 +0800

    refactor: 0845_LongestMountainInArray

--- 0845_LongestMountainInArray/index.js
+++ 0845_LongestMountainInArray/index.js
@@ -5,57 +5,83 @@
  * 1. B.length >= 3
  * 2. There exists some 0 < i < B.length - 1 such that B[0] < B[1] < ... B[i-1] < B[i] > B[i+1] > ... > B[B.length - 1]
  */
-function longestMountain(A) {
-  const ALength = A.length
+// time: O(n)
+// space: O(1)
+function longestMountain(arr) {
   let result = 0
-  let start = 0
-  let end = 1
-  let up = false
-  let down = false
 
-  while (end < ALength) {
-    let curA = A[end]
-    let preA = A[end - 1]
-    console.log('==========')
-    console.log('pre', preA)
-    console.log('cur', curA)
-    if (curA === preA) {
-      up = false
-      down = false
-      // not uphill, reset start
-      start = end
-    } else if (curA > preA) {
-      up = true
-      if (down) {
-        // encounter another uphill, so move left ptr to reset start
-        start = end - 1
-        down = false
-      }
-    } else {
-      if (up && !down) {
-        down = true
-      }
-      if (!up) {
-        // not uphill, reset start
-        start = end
-      }
+  for (let i = 0; i < arr.length; i++) {
+    const isPeak = arr[i - 1] < arr[i] && arr[i + 1] < arr[i]
+    if (!isPeak) continue
+
+    let left = i - 2
+    while (arr[left] < arr[left + 1]) {
+      left--
     }
-    console.log('up', up)
-    console.log('down', down)
-    console.log('start', start)
-    console.log('end', end)
-    if (up && down) {
-      result = Math.max(result, end - start + 1)
+
+    let right = i + 2
+    while (arr[right] < arr[right - 1]) {
+      right++
     }
-    end++
+
+    const currentLength = right - left - 1 // (-2 + 1)
+    result = Math.max(result, currentLength)
   }
 
   return result
 }
 
+// function longestMountain(A) {
+//   const ALength = A.length
+//   let result = 0
+//   let start = 0
+//   let end = 1
+//   let up = false
+//   let down = false
+
+//   while (end < ALength) {
+//     let curA = A[end]
+//     let preA = A[end - 1]
+//     console.log('==========')
+//     console.log('pre', preA)
+//     console.log('cur', curA)
+//     if (curA === preA) {
+//       up = false
+//       down = false
+//       // not uphill, reset start
+//       start = end
+//     } else if (curA > preA) {
+//       up = true
+//       if (down) {
+//         // encounter another uphill, so move left ptr to reset start
+//         start = end - 1
+//         down = false
+//       }
+//     } else {
+//       if (up && !down) {
+//         down = true
+//       }
+//       if (!up) {
+//         // not uphill, reset start
+//         start = end
+//       }
+//     }
+//     console.log('up', up)
+//     console.log('down', down)
+//     console.log('start', start)
+//     console.log('end', end)
+//     if (up && down) {
+//       result = Math.max(result, end - start + 1)
+//     }
+//     end++
+//   }
+
+//   return result
+// }
+
 // O(n)
 
-console.log(longestMountain([2, 1, 4, 7, 3, 2, 5]))
+// console.log(longestMountain([2, 1, 4, 7, 3, 2, 5]))
 
 // ==========
 // pre 2
