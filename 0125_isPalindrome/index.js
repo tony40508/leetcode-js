122a0ee86e17913faf32f8e0c0d6e0e3c15e9f56
Wei Chen <tony40508@gmail.com>
2021-09-25 04:46:31 +0800

    refactor: and test

--- 0125_isPalindrome/index.js
+++ 0125_isPalindrome/index.js
@@ -1,77 +1,27 @@
-const isPalindrome = s => {
-  if (s.length < 0) return
-  const validStr = s.toLowerCase().replace(/[^\w]/g, '')
-  const reversedStr = validStr
-    .split('')
-    .reverse()
-    .join('')
-  return validStr === reversedStr
-}
-
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
 function isPalindrome(s) {
-  if (s.length < 0) return
+  const str = s.toLowerCase().replace(/[^0-9a-zA-Z]/g, '') // [\W] === /[^A-Za-z0-9_]/
+
   let start = 0
-  let end = s.length - 1
+  let end = str.length - 1
 
   while (start < end) {
-    const startStr = s[start].toLowerCase().replace(/[^\w]/g, '')
-    const endStr = s[end].toLowerCase().replace(/[^\w]/g, '')
-    console.log('start:', start, startStr)
-    console.log('end: ', end, endStr)
-
-    if (!startStr) {
-      start++
-      continue
-    }
-    if (!endStr) {
-      end--
-      continue
-    }
-    if (startStr !== endStr) return false
-
+    if (str[start] !== str[end]) return false
     start++
     end--
   }
+
   return true
 }
 
-// "A man, a plan, a canal: Panama"
+export default isPalindrome
 
-// start: 0 a
-// end:  29 a
-// start: 1
-// end:  28 m
-// start: 2 m
-// end:  28 m
-// start: 3 a
-// end:  27 a
-// start: 4 n
-// end:  26 n
-// start: 5
-// end:  25 a
-// start: 6
-// end:  25 a
-// start: 7 a
-// end:  25 a
-// start: 8
-// end:  24 p
-// start: 9 p
-// end:  24 p
-// start: 10 l
-// end:  23
-// start: 10 l
-// end:  22
-// start: 10 l
-// end:  21 l
-// start: 11 a
-// end:  20 a
-// start: 12 n
-// end:  19 n
-// start: 13
-// end:  18 a
-// start: 14
-// end:  18 a
-// start: 15 a
-// end:  18 a
-// start: 16
-// end:  17 c
+// const isPalindrome = (s) => {
+//   if (s.length < 0) return
+//   const validStr = s.toLowerCase().replace(/[^\w]/g, '')
+//   const reversedStr = validStr.split('').reverse().join('')
+//   return validStr === reversedStr
+// }
---
+++ 0125_isPalindrome/index.test.js
@@ -0,0 +1,5 @@
+import isPalindrome from './index'
+
+it('should replace under line', () => {
+  expect(isPalindrome('ab_a')).toBeTruthy()
+})
