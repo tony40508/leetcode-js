4b56e9e6064d3dc972bb162badb572e7dae8a1e0
Wei Chen <tony40508@gmail.com>
2021-10-08 02:50:18 +0800

    refactor: 0098_ValidateBinarySearchTree

--- 0098_ValidateBinarySearchTree/index.js
+++ 0098_ValidateBinarySearchTree/index.js
@@ -1,17 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
 function isValidBST(root) {
   function isValid(root, min, max) {
-    // must at first line to prevent null
     if (!root) return true
+
     const val = root.val
     const left = root.left
     const right = root.right
     if (val <= min || val >= max) return false
     // left should be smaller than val(parent), so put val to max
     // right should be bigger than val(parent), so put val to min
-    if (!isValid(left, min, val)) return false
-    if (!isValid(right, val, max)) return false
-    return true
+    return isValid(left, min, val) && isValid(right, val, max)
   }
-  // return isValid(root, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)
-  return isValid(root)
+
+  return isValid(root, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)
 }
