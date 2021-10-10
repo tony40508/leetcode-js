94700c4da4ebb56ccaada3ac8ee5f58fa7e269c5
Wei Chen <tony40508@gmail.com>
2021-10-11 01:33:40 +0800

    refactor: 0912_SortAnArray

--- 0912_SortAnArray/index.js
+++ 0912_SortAnArray/index.js
@@ -1,50 +1,82 @@
-// function sortArray(nums) {
-//   const length = nums.length
-//   if (length < 2) {
-//     return nums
-//   }
-//   const pivotIndex = length - 1
-//   const pivot = nums[pivotIndex]
-//   let left = []
-//   let right = []
-
-//   for (let i = 0; i < pivotIndex; i++) {
-//     if (nums[i] < pivot) {
-//       left.push(nums[i])
-//     } else {
-//       right.push(nums[i])
-//     }
-//   }
-//   return [...sortArray(left), pivot, ...sortArray(right)]
-// }
-
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
 function sortArray(nums) {
-  function quickSort(array, left, right) {
-    if (left < right) {
-      const pivotIndex = partition(array, left, right)
-      quickSort(array, left, pivotIndex - 1)
-      quickSort(array, pivotIndex + 1, right)
-    }
-  }
-  function partition(array, left, right) {
-    let pivotIndex = left - 1 // lowest index
-    const pivot = array[right]
-    for (let i = left; i < right; i++) {
-      if (array[i] <= pivot) {
-        // if array[i] === pivot also need to pivotIndex++
-        pivotIndex++
-        ;[array[i], array[pivotIndex]] = [array[pivotIndex], array[i]]
-      }
-    }
-    // pivotIndex++ must bigger than pivot, so swap them
-    // e.g [3, 1, 4, 2, 9, 5] pivotIndex here is `3`
-    // so pivotIndex++ then get `9` must bigger than 5
-    pivotIndex++
-    ;[array[pivotIndex], array[right]] = [array[right], array[pivotIndex]]
-    return pivotIndex
-  }
-  quickSort(nums, 0, nums.length - 1)
-  return nums
+  return quickSortInPlace(nums)
+  // return quickSort(nums)
+  // return bubbleSort(nums)
 }
 
-console.log(sortArray([5, 2, 3, 1]))
+function bubbleSort(nums) {
+  const array = nums.slice()
+  let swapped = true
+
+  while (swapped) {
+    swapped = false
+    for (let i = 0; i < array.length - 1; i++) {
+      const j = i + 1
+      if (array[i] > array[j]) {
+        const tmp = array[j]
+        array[j] = array[i]
+        array[i] = tmp
+        swapped = true
+      }
+    }
+  }
+
+  return array
+}
+
+// heap out of memory
+function quickSort(nums) {
+  if (nums.length < 2) return nums
+
+  const array = nums.slice()
+  const pivotIndex = array.length - 1
+  const pivot = array[pivotIndex]
+  const left = []
+  const right = []
+
+  for (let i = 0; i < pivotIndex; i++) {
+    if (array[i] < pivot) {
+      left.push(array[i])
+    } else {
+      right.push(nums[i])
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)]
+}
+
+// In-Place
+function quickSortInPlace(nums) {
+  function quickSort(left, right) {
+    if (left < right) {
+      const pivotIndex = partition(left, right)
+      quickSort(left, pivotIndex - 1)
+      quickSort(pivotIndex + 1, right)
+    }
+  }
+
+  // https://shubo.io/quick-sort/
+  function partition(left, right) {
+    let pivotIndex = left
+    const pivot = nums[right]
+
+    for (let i = left; i < right; i++) {
+      if (nums[i] < pivot) {
+        ;[nums[i], nums[pivotIndex]] = [nums[pivotIndex], nums[i]]
+        pivotIndex++
+      }
+    }
+    // move pivot(nums[right]) to the middle
+    ;[nums[pivotIndex], nums[right]] = [nums[right], nums[pivotIndex]]
+
+    return pivotIndex
+  }
+
+  quickSort(0, nums.length - 1)
+
+  return nums
+}
