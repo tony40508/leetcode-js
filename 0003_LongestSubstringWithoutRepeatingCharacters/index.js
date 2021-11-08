fdcb775a36e10317896d2cd505e181ccb80c0078
Wei Chen <tony40508@gmail.com>
2021-11-09 02:49:06 +0800

    refactor: 0003_LongestSubstringWithoutRepeatingCharacters

--- 0003_LongestSubstringWithoutRepeatingCharacters/index.js
+++ 0003_LongestSubstringWithoutRepeatingCharacters/index.js
@@ -3,94 +3,42 @@
  * @return {number}
  */
 function lengthOfLongestSubstring(s) {
-  const slideWindow = new Map()
-  let start = 0
-  let end = 0
-  let result = 0
+  let currentSubstring = ''
+  let longestLength = 0
 
-  while (end < s.length) {
-    console.log('==========')
-    const currentStr = s[end]
-    if (slideWindow.has(currentStr)) {
-      const repeatedIndex = slideWindow.get(currentStr)
-      // reset start index
-      // make sure repeatedIndex < start (line 49)
-      start = repeatedIndex < start ? start : repeatedIndex + 1
-      console.log('repeatedIndex', repeatedIndex)
+  for (const char of s) {
+    const index = currentSubstring.indexOf(char)
+
+    if (index > -1) {
+      currentSubstring = currentSubstring.slice(index + 1) // remove repetitive char so beginIndex is index + 1
     }
-    slideWindow.set(currentStr, end)
-    result = Math.max(result, end - start + 1)
-    // calcutate result first then end++
-    end++
-    console.log('start', start)
-    console.log('end', end)
-    console.log(slideWindow)
+    currentSubstring += char
+
+    longestLength = Math.max(longestLength, currentSubstring.length)
   }
 
-  return result
+  return longestLength
 }
 
-// console.log(lengthOfLongestSubstring('abba'))
+// function lengthOfLongestSubstring(s) {
+//   const slideWindow = new Map()
+//   let start = 0
+//   let end = 0
+//   let result = 0
 
-// ==========
-// start 0
-// end 1
-// Map { 'a' => 0 }
-// ==========
-// start 0
-// end 2
-// Map { 'a' => 0, 'b' => 1 }
-// ==========
-// repeatedIndex 1
-// start 2
-// end 3
-// Map { 'a' => 0, 'b' => 2 }
-// ==========
-// repeatedIndex 0 (repeatedIndex has possibility less than start)
-// start 2
-// end 4
-// Map { 'a' => 3, 'b' => 2 }
-//
-// 2
+//   while (end < s.length) {
+//     const currentStr = s[end]
+//     if (slideWindow.has(currentStr)) {
+//       const repeatedIndex = slideWindow.get(currentStr)
+//       // reset start index
+//       // make sure repeatedIndex < start
+//       start = repeatedIndex < start ? start : repeatedIndex + 1
+//     }
+//     slideWindow.set(currentStr, end)
+//     result = Math.max(result, end - start + 1)
+//     // calcutate result first then end++
+//     end++
+//   }
 
-console.log(lengthOfLongestSubstring('abcabcbb'))
-
-// ==========
-// start 0
-// end 1
-// Map { 'a' => 0 }
-// ==========
-// start 0
-// end 2
-// Map { 'a' => 0, 'b' => 1 }
-// ==========
-// start 0
-// end 3
-// Map { 'a' => 0, 'b' => 1, 'c' => 2 }
-// ==========
-// repeatedIndex 0
-// start 1
-// end 4
-// Map { 'a' => 3, 'b' => 1, 'c' => 2 }
-// ==========
-// repeatedIndex 1
-// start 2
-// end 5
-// Map { 'a' => 3, 'b' => 4, 'c' => 2 }
-// ==========
-// repeatedIndex 2
-// start 3
-// end 6
-// Map { 'a' => 3, 'b' => 4, 'c' => 5 }
-// ==========
-// repeatedIndex 4
-// start 5
-// end 7
-// Map { 'a' => 3, 'b' => 6, 'c' => 5 }
-// ==========
-// repeatedIndex 6
-// start 7
-// end 8
-// Map { 'a' => 3, 'b' => 7, 'c' => 5 }
-//
-// 3
+//   return result
+// }
