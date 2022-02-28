f71110f1d9d76e884c69bfa1425c17af5c06fca9
Wei Chen <tony40508@gmail.com>
2022-02-28 14:36:43 +0800

    refactor: 0026_removeDuplicates

--- 0026_removeDuplicates/index.js
+++ 0026_removeDuplicates/index.js
@@ -1,3 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function removeDuplicates(nums) {
+  let uniqueNum
+  let lastIndex = 0
+  for (const num of nums) {
+    if (num !== uniqueNum) {
+      uniqueNum = num
+      nums[lastIndex] = num
+      lastIndex++
+    }
+  }
+  return lastIndex
+}
+
+// To avoid reorder the array, we should splice the array from last item
+const removeDuplicates = (nums) => {
+  let lastIndex = nums.length - 1
+  for (let i = lastIndex; i >= 0; i--) {
+    if (nums[i] === nums[i - 1]) {
+      nums.splice(i, 1)
+    }
+  }
+  return nums.length
+}
+
 // const removeDuplicates = nums => {
 //   for (let i = 0; i < nums.length - 1; i++) {
 //     if (nums[i] === nums[i + 1]) {
@@ -6,13 +34,3 @@
 //     }
 //   }
 // }
-
-// To avoid reorder the array, we should splice the array from last item
-const removeDuplicates = nums => {
-  let lastIndex = nums.length - 1
-  for (let i = lastIndex; i >= 0; i--) {
-    if (nums[i] === nums[i - 1]) {
-      nums.splice(i, 1)
-    }
-  }
-}
