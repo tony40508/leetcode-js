5527a1d9974a681e195adfe12492275de46c4908
Wei Chen <tony40508@gmail.com>
2022-02-28 15:58:07 +0800

    feat(mid): 0006_ZigzagConversion

---
+++ 0006_ZigzagConversion/index.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+function convert(s, numRows) {
+  if (numRows === 1) return s
+
+  let result = ''
+
+  for (let row = 0; row < numRows; row++) {
+    const gap = numRows + numRows - 2
+    for (let i = row; i < s.length; i += gap) {
+      result += s[i]
+
+      // skip row of top/bottom
+      if (row > 0 && row < numRows - 1) {
+        let midIndex = i + gap - row * 2
+        if (s[midIndex]) {
+          result += s[midIndex]
+        }
+      }
+    }
+  }
+
+  return result
+}
+
+// numRows = 5
+// P       H
+// A     S
+// Y   I
+// P L
+// A
