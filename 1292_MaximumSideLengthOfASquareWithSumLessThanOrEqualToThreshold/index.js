cc4d18a8615cf42595c3a4f6d62f2bea50aa8ae2
Wei Chen <tony40508@gmail.com>
2022-04-23 18:36:38 +0800

    feat(mid): 1292

---
+++ 1292_MaximumSideLengthOfASquareWithSumLessThanOrEqualToThreshold/index.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[][]} mat
+ * @param {number} threshold
+ * @return {number}
+ */
+function maxSideLength(mat, threshold) {
+  const m = mat.length
+  const n = mat[0].length
+
+  const dp = [] // (m+1)x(n+1)
+  for (let i = 0; i <= m; i++) {
+    dp[i] = new Array(n + 1).fill(0)
+  }
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      dp[i][j] =
+        dp[i - 1][j] + dp[i][j - 1] + mat[i - 1][j - 1] - dp[i - 1][j - 1]
+    }
+  }
+
+  const getSum = (i, j, length) =>
+    dp[i][j] -
+    dp[i - length][j] -
+    dp[i][j - length] +
+    dp[i - length][j - length]
+
+  let maxSideLength = 0
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      let curSideLength = maxSideLength
+      while (i - curSideLength >= 0 && j - curSideLength >= 0) {
+        if (getSum(i, j, curSideLength) > threshold) break
+        maxSideLength = Math.max(maxSideLength, curSideLength)
+        curSideLength++
+      }
+    }
+  }
+
+  return maxSideLength
+}
