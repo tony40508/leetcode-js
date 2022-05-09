45604d2af02752b1542e164f57ef5fed9f6fa966
Wei Chen <tony40508@gmail.com>
2022-05-10 00:35:41 +0800

    refactor: 0008_stringToInt

--- 0008_stringToInt/index.js
+++ 0008_stringToInt/index.js
@@ -3,38 +3,51 @@
  * @return {number}
  */
 function myAtoi(str) {
+  const num = parseInt(str)
+
+  if (Number.isNaN(num)) return 0
+
   const min = -Math.pow(2, 31)
   const max = Math.pow(2, 31) - 1
-  let i = 0
-  let sign = 1
-  let ans = 0
 
-  // should use while rather than if
-  while (str[i] === ' ') {
-    i++
-  }
+  if (num > max) return max
+  if (num < min) return min
 
-  if (str[i] === '+') {
-    i++
-  } else if (str[i] === '-') {
-    sign = -1
-    i++
-  }
-
-  // /[0-9]/.test(s[i])
-  while (str[i] >= '0' && str[i] <= '9') {
-    ans = ans * 10 + parseInt(str[i], 10)
-
-    if (ans * sign >= max) {
-      return max
-    } else if (ans * sign <= min) {
-      return min
-    }
-
-    i++
-  }
-
-  return ans * sign
+  return num
 }
+// function myAtoi(str) {
+//   const min = -Math.pow(2, 31)
+//   const max = Math.pow(2, 31) - 1
+//   let i = 0
+//   let sign = 1
+//   let ans = 0
+
+//   // should use while rather than if
+//   while (str[i] === ' ') {
+//     i++
+//   }
+
+//   if (str[i] === '+') {
+//     i++
+//   } else if (str[i] === '-') {
+//     sign = -1
+//     i++
+//   }
+
+//   // /[0-9]/.test(s[i])
+//   while (str[i] >= '0' && str[i] <= '9') {
+//     ans = ans * 10 + parseInt(str[i], 10)
+
+//     if (ans * sign >= max) {
+//       return max
+//     } else if (ans * sign <= min) {
+//       return min
+//     }
+
+//     i++
+//   }
+
+//   return ans * sign
+// }
 
 export { myAtoi }
